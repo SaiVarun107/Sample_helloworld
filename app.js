7b3499d9c9c263614cfb91fbeadf5a02ce48320a
SaiVarun107 <saivarunbitra107@gmail.com>
2026-05-04 13:29:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.write("Hello World!");
-  res.write("Hello World!");
-  res.end();
+  res.end("Hello World!");
 });
 
 server.listen(3000, () => {
